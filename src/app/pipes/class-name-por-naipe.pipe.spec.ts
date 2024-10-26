2093b255d96df4ef0b18d17def84dfa023e38e10
Gustavo Buratto <gustavo_buratto@hotmail.com>
2024-10-25 21:04:08 -0300

    Implementa os testes unitários do `ClassNamePorNaipePipe`

--- src/app/pipes/class-name-por-naipe.pipe.spec.ts
+++ src/app/pipes/class-name-por-naipe.pipe.spec.ts
@@ -1,8 +1,36 @@
 import { ClassNamePorNaipePipe } from './class-name-por-naipe.pipe';
 
 describe('ClassNamePorNaipePipe', () => {
+
+  let pipe: ClassNamePorNaipePipe;
+
+  beforeEach(() => {
+    pipe = new ClassNamePorNaipePipe();
+  });
+
   it('create an instance', () => {
     const pipe = new ClassNamePorNaipePipe();
     expect(pipe).toBeTruthy();
   });
+
+  describe('transform', () => {
+
+    it('deve retornar "vermelho" para naipe "copas"', () => {
+      expect(pipe.transform('copas')).toBe('vermelho');
+    });
+
+    it('deve retornar "vermelho" para naipe "ouro"', () => {
+      expect(pipe.transform('ouro')).toBe('vermelho');
+    });
+
+    it('deve retornar "preto" para naipe "espadas"', () => {
+      expect(pipe.transform('espadas')).toBe('preto');
+    });
+
+    it('deve retornar "preto" para naipe "paus"', () => {
+      expect(pipe.transform('paus')).toBe('preto');
+    });
+
+  });
+
 });
